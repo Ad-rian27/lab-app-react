e3cc66044ef8d4f6efd7b9549d2336e2623262a4
Ad-rian27 <rian270627@gmail.com>
2026-07-14 15:01:02 +0530

    Added ViewEntry component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import NavigationBar from './components/NavigationBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ViewEntry from './components/ViewEntry'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<ViewEntry />} />
+  <Route path='/view' element={<ViewEntry />} />
+</Routes>
+</BrowserRouter>
     </>
   )
 }
